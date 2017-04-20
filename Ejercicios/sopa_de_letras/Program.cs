ac5aec3ae6e0af0e9c0906bd73bdd01b17926480
Daniel Gallardo <aslan_m_d@hotmail.com>
2017-04-20 21:19:42 +0200

    implementacion simple

--- Ejercicios/sopa_de_letras/Program.cs
+++ Ejercicios/sopa_de_letras/Program.cs
@@ -19,6 +19,7 @@ namespace sopa_de_letras
 			s.matriz = new String[]{"ABCDBARCO" , "EKLMNOPQR" , "HTAVIONOR" , "CGRTUITXB" , "OROHFOVAZ" , "CMPPMEVAN"};
 			string[] pals = {"COCHE", "AVION", "BARCO", "MOTO", "PATINES"};
 			Resuelve (ref s, pals);
+			Console.ReadLine();
 		}
 
 		public static Par[] dirs(){
@@ -27,7 +28,6 @@ namespace sopa_de_letras
 				new Par{x=-1, y= 0},
 				new Par{x=-1, y= 1},
 				new Par{x= 0, y=-1},
-				new Par{x= 0, y= 0},
 				new Par{x= 0, y= 1},
 				new Par{x= 1, y=-1},
 				new Par{x= 1, y= 0},
@@ -38,37 +38,65 @@ namespace sopa_de_letras
 		public static bool compruebaPosDir(ref Sopa s, string pal, Par pos, Par dir){
 			bool encontrado = true;
 			int indice = 0;
+
+			int x = pos.x;
+			int y = pos.y;
 			while (encontrado && indice < pal.Length) {
-				encontrado = s.matriz[dir.x % s.ancho, dir.y % s.alto] = pal[indice];
+				if (x < 0) { x = s.alto - 1; } else if(x >= s.alto) {x = 0;}
+				if (y < 0) { y = s.ancho - 1; } else if(y >= s.ancho) { y = 0; }
+				encontrado = s.matriz[x][y] == pal[indice];
 				indice++;
-				dir.x += dir.x;
-				dir.y += dir.y;
+				x += dir.x;
+				y += dir.y;
 			}
+
 			return encontrado;
 		}
 
 		public static bool buscaDir(ref Sopa s, string pal, Par pos, out Par dir){
-			
-			return false;
+			Par[] direcciones = dirs ();
+			bool dirEncontrada = false;
+			int indice = 0;
+			dir = new Par{ x=0,y=0};
+			while (!dirEncontrada && indice < direcciones.Length) {
+				Par dirActual = direcciones [indice++];
+				if (compruebaPosDir (ref s, pal, pos, dirActual)) {
+					dir = dirActual;
+					dirEncontrada = true;
+				}
+			}
+			return dirEncontrada;
 		}
 
-		public static bool buscaPal(ref Sopa s, string pal, Par pos, Par dir){
-
-			return false;
+		public static bool buscaPal(ref Sopa s, string pal, out Par pos,out Par dir){
+			Par p = new Par{x = 0, y = 0};
+			bool posEncontrada = false;
+			pos = p;
+			dir = p;
+			while(p.x < s.alto && !posEncontrada){
+				p.y = 0;
+				while (p.y < s.ancho && !posEncontrada) {
+					if (buscaDir (ref s, pal, p, out dir)) {
+						pos = p;
+						posEncontrada = true;
+					}
+					p.y++;
+				}
+				p.x++;
+			}
+			return posEncontrada;
 		}
 
 		public static void Resuelve(ref Sopa s, string[] pals){
 			for (int i = 0; i < pals.Length; i++) {
-				string actual = pals [i];
-				Par dir;
-				for (int j = 0; j < dirs().Length; j++) {
-					dir = dirs()[j];
-					if(buscaDir (s, actual, pos, out dir)){
-						if (buscaPal (s, actual, pos, dir)) {
-							Console.WriteLine ("Encontrada "+ pals[i] + "en posicion ("+ x + ", " + y + ")" + "direccion (" + dir.x + ", " +dir.y + ")");
-						}
-					}
+				string palabraActual = pals [i];
+				Par pos, dir;
+				if(buscaPal(ref s, palabraActual, out pos, out dir)){
+					Console.WriteLine ("Encontrada "+ pals[i] + 
+					                   " en posicion ("+ pos.x + ", " + pos.y + ")" + 
+					                   " direccion (" + dir.x + ", " +dir.y + ")");
 				}
+
 			}
 
 		}
