61e7711acbd9a5efd50b04a7b8d560efa0b4a395
Daniel Gallardo <aslan_m_d@hotmail.com>
2017-04-20 10:01:32 +0200

    estructura inicial

--- Ejercicios/sopa_de_letras/Program.cs
+++ Ejercicios/sopa_de_letras/Program.cs
@@ -2,11 +2,39 @@
 
 namespace sopa_de_letras
 {
-	class MainClass
-	{
-		public static void Main (string[] args)
-		{
-			Console.WriteLine ("Hello World!");
+	struct Par{
+		public int x, y;
+	}
+
+	struct Sopa{
+		public int alto, ancho;
+		public string[] matriz;
+	}
+
+	class MainClass {
+		public static void Main (string[] args) {
+			Sopa s;
+			s.alto = 6;
+			s.ancho = 9;
+			s.matriz = new String[]{"ABCDBARCO" , "EKLMNOPQR" , "HTAVIONOR" , "CGRTUITXB" , "OROHFOVAZ" , "CMPPMEVAN"};
+			string[] pals = {"COCHE", "AVION", "BARCO", "MOTO", "PATINES"};
+			Resuelve (s, pals);
+		}
+
+		public static Par[] dirs(){
+			
+		}
+
+		public static bool compruebaPosDir(Sopa s, string pal, Par pos, Par dir){
+
+		}
+
+		public static bool buscaDir(Sopa s, string pal, Par pos, Par dir){
+			
+		}
+
+		public static void Resuelve(Sopa s, string[] pals){
+			
 		}
 	}
 }
